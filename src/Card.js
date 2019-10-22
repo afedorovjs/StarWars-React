c473e836c89c1341c6169684547f5e4aa57fcb57
Nikita <nikita@shelimov.me>
2019-10-23 01:48:33 +0300

    minor refactoring

--- src/Card.js
+++ src/Card.js
@@ -1,29 +1,42 @@
 import React, { useCallback, useState } from 'react';
 import Modal from './Modal';
 
-
 function fetchData(url) {
-  fetch(url)
-      .then(result => result.json())
-      .then(result => this.setModalData(result))
-      .catch(error => error)
+  return fetch(url).then(result => result.json());
 }
 
 function Card({ name, url }) {
-  const firstLetter = name.slice(0, 1);
+  const firstLetter = name[0];
   const [isOpen, setIsOpen] = useState(false);
+  const [fetchResponse, setFetchResponse] = useState({ data: null, error: false });
 
-  const onOpen = useCallback(
-    () => {
-      setIsOpen(!isOpen);
-    },
-    [isOpen]
+  const openModal = useCallback(() => {
+    setIsOpen(true);
+    if (fetchResponse.data === null) {
+      fetchData(url).then(
+        (resp) => {
+          setFetchResponse({ data: resp });
+        },
+        (error) => {
+          setFetchResponse({ data: null, error: `Some error occured ${error.name}`});
+        }
+      );
+    }
+  }, [setFetchResponse, fetchResponse, url]);
+
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
+
+  const needRenderModal = (
+    (fetchResponse.data !== null || !fetchResponse.error) &&
+    isOpen
   );
 
   return (
     <>
       <li>
-        <div className="card" onClick={onOpen}>
+        <div className="card" onClick={openModal}>
           <div className="avatarWrapper">
             <div className="avatar">{ firstLetter }</div>
             <p className="avatarName">{ name }</p>
@@ -31,12 +44,12 @@ function Card({ name, url }) {
           </div>
         </div>
       </li>
-      { isOpen ? (
-          <Modal
-
-          />
-        ) : null
-      }
+      {needRenderModal && (
+        <Modal
+          fetchResponse={fetchResponse}
+          onClose={closeModal}
+        />
+      )}
     </>
   );
 }
--- src/Modal.js
+++ src/Modal.js
@@ -3,51 +3,54 @@ import Portal from './Portal';
 
 import './Modal.css';
 
-function Modal({ onClose, name, }) {
+function Modal({ onClose, name, fetchResponse }) {
+  if (fetchResponse.error) {
+    return <Portal>Error!</Portal>
+  }
 
   return (
-        <Portal>
-            <div className="modalWrapper">
-              <div className="modalWindow">
-                <button className="modalCloseBtn" onClick={onClose}/>
-                <div className="modalHeader">
-                  <div className="modalAvatar">N</div>
-                  <p>name</p>
-                </div>
-                <div className="modalBody">
-                  <div className="threeWrapper">
-                    <div className="titlesWrapper">
-                      <span className="iconDOB"/>
-                      <span className="titles">Birth year</span>
-                      <span className="titleValue">Birth year</span>
-                    </div>
-                    <div className="titlesWrapper">
-                      <span className="iconSpecies"/>
-                      <span className="titles">Species</span>
-                      <span className="titleValue">Species</span>
-                    </div>
-                    <div className="titlesWrapper">
-                      <span className="iconGender"/>
-                      <span className="titles">Gender</span>
-                      <span className="titleValue">Gender</span>
-                    </div>
-                  </div>
-                  <div className="threeWrapper">
-                    <div className="titlesWrapper">
-                      <span className="iconHomeworld"/>
-                      <span className="titles">Homeworld</span>
-                      <span className="titleValue">Homeworld</span>
-                    </div>
-                    <div className="titlesWrapper">
-                      <span className="iconFilms"/>
-                      <span className="titles">Films</span>
-                      <span className="titleValue">Films</span>
-                    </div>
-                  </div>
-                </div>
+    <Portal>
+      <div className="modalWrapper">
+        <div className="modalWindow">
+          <button className="modalCloseBtn" onClick={onClose}/>
+          <div className="modalHeader">
+            <div className="modalAvatar">N</div>
+            <p>name</p>
+          </div>
+          <div className="modalBody">
+            <div className="threeWrapper">
+              <div className="titlesWrapper">
+                <span className="iconDOB"/>
+                <span className="titles">Birth year</span>
+                <span className="titleValue">Birth year</span>
+              </div>
+              <div className="titlesWrapper">
+                <span className="iconSpecies"/>
+                <span className="titles">Species</span>
+                <span className="titleValue">Species</span>
+              </div>
+              <div className="titlesWrapper">
+                <span className="iconGender"/>
+                <span className="titles">Gender</span>
+                <span className="titleValue">Gender</span>
               </div>
             </div>
-        </Portal>
+            <div className="threeWrapper">
+              <div className="titlesWrapper">
+                <span className="iconHomeworld"/>
+                <span className="titles">Homeworld</span>
+                <span className="titleValue">Homeworld</span>
+              </div>
+              <div className="titlesWrapper">
+                <span className="iconFilms"/>
+                <span className="titles">Films</span>
+                <span className="titleValue">Films</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Portal>
   )
 }
 
