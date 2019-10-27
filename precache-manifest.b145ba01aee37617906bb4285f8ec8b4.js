f8d11b2dccaf27d917a57389224ec650c354e6b8
Aleksandr Fedorov <nsgowest@gmail.com>
2019-10-27 22:24:44 +0300

    Updates

---
+++ precache-manifest.b145ba01aee37617906bb4285f8ec8b4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42500d6dc828a499ee74c4fe3db5adf5",
+    "url": "/StarWars-React/index.html"
+  },
+  {
+    "revision": "e107acae7d0e5f4fabe7",
+    "url": "/StarWars-React/static/css/main.85355bee.chunk.css"
+  },
+  {
+    "revision": "b892e9e8325fb33fbfaf",
+    "url": "/StarWars-React/static/js/2.02dc0c17.chunk.js"
+  },
+  {
+    "revision": "e107acae7d0e5f4fabe7",
+    "url": "/StarWars-React/static/js/main.2ce02a68.chunk.js"
+  },
+  {
+    "revision": "4b1e390d856b7ed8969e",
+    "url": "/StarWars-React/static/js/runtime-main.dbee025f.js"
+  },
+  {
+    "revision": "635debde475ce214cedfebb15a308836",
+    "url": "/StarWars-React/static/media/DOB.635debde.svg"
+  },
+  {
+    "revision": "fa5a78bd0ebf778da4c2031f37efab56",
+    "url": "/StarWars-React/static/media/Gender.fa5a78bd.svg"
+  },
+  {
+    "revision": "c112195ec02702219da7bd063c5beb54",
+    "url": "/StarWars-React/static/media/Species.c112195e.svg"
+  },
+  {
+    "revision": "400c387848e878f959e5a8623b350059",
+    "url": "/StarWars-React/static/media/close-btn.400c3878.svg"
+  },
+  {
+    "revision": "5b35eabef593f79983e9f2edf26e940a",
+    "url": "/StarWars-React/static/media/falcon.5b35eabe.svg"
+  },
+  {
+    "revision": "fa434cbab55418cab204646f07b80095",
+    "url": "/StarWars-React/static/media/films.fa434cba.svg"
+  },
+  {
+    "revision": "dfc2e198a45524c4e1c0a8a1ec16a391",
+    "url": "/StarWars-React/static/media/header-background.dfc2e198.jpg"
+  },
+  {
+    "revision": "9354abd515c993187e91f28acb866084",
+    "url": "/StarWars-React/static/media/home-world.9354abd5.svg"
+  },
+  {
+    "revision": "05e75c87254a5fd8c9339e3acb3e0f7b",
+    "url": "/StarWars-React/static/media/ic_search_24px.05e75c87.svg"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "260c8072feca314957defd2ad4ccc0ee",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-500.260c8072.eot"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-500.87284894.woff"
+  },
+  {
+    "revision": "8c608256fb0273e3a36e6b603f71f213",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-500.8c608256.ttf"
+  },
+  {
+    "revision": "95204ac95130828753c0ee0ada537c33",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-500.95204ac9.svg"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "4be1a572fca40bcb2202504cb17aed91",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-regular.4be1a572.eot"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/StarWars-React/static/media/roboto-v20-latin-regular.8681f434.svg"
+  },
+  {
+    "revision": "539b44927529f293d12092a21a1660dc",
+    "url": "/StarWars-React/static/media/star.539b4492.svg"
+  },
+  {
+    "revision": "584c004dbc6c7696861ccf8f4d7e4303",
+    "url": "/StarWars-React/static/media/wars.584c004d.svg"
+  }
+]);
