f5d46093f459c9545f109ccd327e940ec121eea2
Aleksandr Fedorov <afedorov@spbfiller.com>
2020-12-14 00:33:38 +0300

    Updates

--- precache-manifest.eea2004e5d070bf38667144b86ef7f57.js
+++ precache-manifest.eea2004e5d070bf38667144b86ef7f57.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0246b686ed01a42b1e844549ee6fbb4e",
+    "revision": "dfb832234ab3e72cadc5990b636b7681",
     "url": "/StarWars-React/index.html"
   },
   {
-    "revision": "924a2f0d0526f6339b3d",
+    "revision": "16d0d548be16a3d6742d",
     "url": "/StarWars-React/static/css/main.8a9ac6e6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/StarWars-React/static/js/2.1c078fc0.chunk.js"
   },
   {
-    "revision": "924a2f0d0526f6339b3d",
-    "url": "/StarWars-React/static/js/main.50dfc83c.chunk.js"
+    "revision": "16d0d548be16a3d6742d",
+    "url": "/StarWars-React/static/js/main.733568be.chunk.js"
   },
   {
     "revision": "4b1e390d856b7ed8969e",
